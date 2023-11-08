69a4d1a6b12a54e3df285f21a7dd98a31143493f
splconline <63439754+splconline@users.noreply.github.com>
2023-11-09 07:50:58 +0800

    Create languages-profit.sql

---
+++ term-report/sql/languages-profit.sql
@@ -0,0 +1,10 @@
+SELECT Course,Code,
+    CAST(replace(substr(TotalIncome,2,length(TotalIncome)),",","") AS FLOAT) AS Income,
+    CAST(replace(substr(OperatingFee,2,length(OperatingFee)),",","") AS FLOAT) AS Fee,
+	CAST(replace(substr(TotalIncome,2,length(TotalIncome)),",","") AS FLOAT) -
+	CAST(replace(substr(OperatingFee,2,length(OperatingFee)),",","") AS FLOAT) AS Profit
+FROM
+    course_incomeT3
+WHERE
+    substr(Code,1,1)="L"
+ORDER BY Profit
