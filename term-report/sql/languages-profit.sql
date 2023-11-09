5ea705ec7b89520379a55179c06bf4a617743777
splconline <63439754+splconline@users.noreply.github.com>
2023-11-09 20:25:09 +0800

    Update languages-profit.sql

--- term-report/sql/languages-profit.sql
+++ term-report/sql/languages-profit.sql
@@ -1,15 +1,12 @@
-SELECT Course,Code, TotStus, FirstSession,
+SELECT Code, Course, TotStus,
     CAST(replace(substr(TotalIncome,2),",","") AS FLOAT) AS Income,
     CAST(replace(substr(OperatingFee,2),",","") AS FLOAT) AS Fee,
  	CAST(replace(substr(TotalIncome,2),",","") AS FLOAT) -
   	CAST(replace(substr(OperatingFee,2),",","") AS FLOAT) AS Profit,
-	substr(FirstSession, 1, instr(FirstSession,' ')) AS TheDate,
---	CASE length(substr(FirstSession, 1, instr(FirstSession,' ')))
---	    WHEN 10 THEN '0' || substr(FirstSession, 1, instr(FirstSession,' '))
---		ELSE substr(FirstSession, 1, instr(FirstSession,' ')) AS ThePadDate
---    END
-	substr(substr(FirstSession, 1, instr(FirstSession,' ')), 8, 4) || '-' || 
-            CASE substr(substr(FirstSession, 1, instr(FirstSession,' ')), 4, 3)
+	CASE length(substr(FirstSession, 1, instr(FirstSession,' ')))
+        WHEN 11 THEN
+	        substr(FirstSession,7,4) || '-' ||
+	        CASE substr(FirstSession,3,3)
                 WHEN 'Jan' THEN '01'
                 WHEN 'Feb' THEN '02'
                 WHEN 'Mar' THEN '03'
@@ -22,10 +19,28 @@ SELECT Course,Code, TotStus, FirstSession,
                 WHEN 'Oct' THEN '10'
                 WHEN 'Nov' THEN '11'
                 WHEN 'Dec' THEN '12'
-            END || '-' || substr(substr(FirstSession, 1, instr(FirstSession,' ')), 1, 2) AS ISODate
-	
+            END || '-0' || substr(FirstSession,1,1)
+	    ELSE
+		    substr(FirstSession,8,4) || '-' ||
+	        CASE substr(FirstSession,4,3)
+                WHEN 'Jan' THEN '01'
+                WHEN 'Feb' THEN '02'
+                WHEN 'Mar' THEN '03'
+                WHEN 'Apr' THEN '04'
+                WHEN 'May' THEN '05'
+                WHEN 'Jun' THEN '06'
+                WHEN 'Jul' THEN '07'
+                WHEN 'Aug' THEN '08'
+                WHEN 'Sep' THEN '09'
+                WHEN 'Oct' THEN '10'
+                WHEN 'Nov' THEN '11'
+                WHEN 'Dec' THEN '12'
+			END	|| '-' || substr(FirstSession,1,2)
+	END AS ISODate
 FROM
-    course_incomeT4
+    course_incomeT3
 WHERE
-    substr(Code,1,1)="L"
+-- for T4: AC, HW, L, SC, SB
+    substr(Code,1,1)="L" AND
+    date(ISODate) > date('2023-07-01')
 ORDER BY Profit
