621750ec88ac5e2ad5c17f751dd72a984275e445
splconline <63439754+splconline@users.noreply.github.com>
2023-11-10 07:59:02 +0800

    Finished combining profit report and facilitator list

--- term-report/sql/by-facilitator.sql
+++ term-report/sql/by-facilitator.sql
@@ -3,11 +3,5 @@ SELECT
 FROM
     course_incomeT4 INNER JOIN course_historyT4
     ON course_incomeT4.Code = course_historyT4.Code
+    AND course_incomeT4.FirstSession = course_historyT4.Session || " " || course_historyT4.Time
 
---      AND course_incomeT4.Date = course_historyT4.Date
---      (but we need to convert the dates to datetime)
---      https://www.internotes.net/sqlite-dates
---      course_incomeT4: FirstSession 16-Oct-2023 10:30am
---      course_historyT4: Session 20-Oct-2023 Time 10:30am
---      So course_incomeT4.FirstSession = course_historyT4.Session || " " || course_historyT4.Time
---      should work
--- term-report/sql/languages-profit.sql
+++ term-report/sql/languages-profit.sql
@@ -1,12 +1,12 @@
-SELECT Code, Course, TotStus,
-    CAST(replace(substr(TotalIncome,2),",","") AS FLOAT) AS Income,
-    CAST(replace(substr(OperatingFee,2),",","") AS FLOAT) AS Fee,
- 	CAST(replace(substr(TotalIncome,2),",","") AS FLOAT) -
-  	CAST(replace(substr(OperatingFee,2),",","") AS FLOAT) AS Profit,
-	CASE length(substr(FirstSession, 1, instr(FirstSession,' ')))
+SELECT course_incomeT4.Code, course_incomeT4.Course, course_incomeT4.TotStus,
+    CAST(replace(substr(course_incomeT4.TotalIncome,2),",","") AS FLOAT) AS Income,
+    CAST(replace(substr(course_incomeT4.OperatingFee,2),",","") AS FLOAT) AS Fee,
+ 	CAST(replace(substr(course_incomeT4.TotalIncome,2),",","") AS FLOAT) -
+  	CAST(replace(substr(course_incomeT4.OperatingFee,2),",","") AS FLOAT) AS Profit,
+	CASE length(substr(course_incomeT4.FirstSession, 1, instr(course_incomeT4.FirstSession,' ')))
         WHEN 11 THEN
-	        substr(FirstSession,7,4) || '-' ||
-	        CASE substr(FirstSession,3,3)
+	        substr(course_incomeT4.FirstSession,7,4) || '-' ||
+	        CASE substr(course_incomeT4.FirstSession,3,3)
                 WHEN 'Jan' THEN '01'
                 WHEN 'Feb' THEN '02'
                 WHEN 'Mar' THEN '03'
@@ -19,10 +19,10 @@ SELECT Code, Course, TotStus,
                 WHEN 'Oct' THEN '10'
                 WHEN 'Nov' THEN '11'
                 WHEN 'Dec' THEN '12'
-            END || '-0' || substr(FirstSession,1,1)
+            END || '-0' || substr(course_incomeT4.FirstSession,1,1)
 	    ELSE
-		    substr(FirstSession,8,4) || '-' ||
-	        CASE substr(FirstSession,4,3)
+		    substr(course_incomeT4.FirstSession,8,4) || '-' ||
+	        CASE substr(course_incomeT4.FirstSession,4,3)
                 WHEN 'Jan' THEN '01'
                 WHEN 'Feb' THEN '02'
                 WHEN 'Mar' THEN '03'
@@ -35,12 +35,14 @@ SELECT Code, Course, TotStus,
                 WHEN 'Oct' THEN '10'
                 WHEN 'Nov' THEN '11'
                 WHEN 'Dec' THEN '12'
-			END	|| '-' || substr(FirstSession,1,2)
-	END AS ISODate
+			END	|| '-' || substr(course_incomeT4.FirstSession,1,2)
+	END AS ISODate, course_historyT4.Facilitator
 FROM
-    course_incomeT3
+    course_incomeT4 INNER JOIN course_historyT4
+    ON course_incomeT4.Code = course_historyT4.Code
+    AND course_incomeT4.FirstSession = course_historyT4.Session || " " || course_historyT4.Time
 WHERE
 -- for T4: AC, HW, L, SC, SB
-    substr(Code,1,1)="L" AND
-    date(ISODate) > date('2023-07-01')
-ORDER BY Profit
+--    substr(course_incomeT4.Code,1,1)="L" AND
+    date(ISODate) > date('2023-09-01')
+ORDER BY Facilitator
