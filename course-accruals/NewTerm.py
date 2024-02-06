d4581575edb427b5887e9febca5aea5050c28708
splconline <63439754+splconline@users.noreply.github.com>
2024-02-06 16:20:51 +0800

    new new month

--- course-accruals/NewTerm.py
+++ course-accruals/NewTerm.py
@@ -1,44 +1,106 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
 import pandas as pd
 from pathlib import Path
 
+
+# In[2]:
+
+
 input_file = Path.cwd()/'course_income.csv'
+
+
+# In[3]:
+
 
 df = pd.read_csv(input_file)
 
-# Lose unwanted columns
 
-df.drop(['Day', 'Start',
-       'Tot-Stus', 'Concession-Stus', 'CM Fee',
-       'Concession Fee','Total Income'], axis=1, inplace=True)
+# In[4]:
+
+
+df
+
+
+# Lose unwanted columns then sort by Course Code
+
+# In[5]:
+
+
+df.drop(['Day', 'Start', 'Concession-Stus', 'CM Fee',
+       'Concession Fee'], axis=1, inplace=True)
+df = df.sort_values('Code')
+
+
+# In[6]:
+
+
+df
+
 
 # Make the '$1,200' strings numeric
+
+# In[7]:
+
 
 df['Operating Fee']=df['Operating Fee'].str.lstrip('$')
 df['Operating Fee']=df['Operating Fee'].str.replace(',','')
 df['Operating Fee']=pd.to_numeric(df['Operating Fee'])
 
+
 # While you're there, fix the dates too
+
+# In[8]:
+
 
 df['First Session'] = pd.to_datetime(df['First Session'])
 
-# Add required blank columns, then reorder all the columns
 
-df['Category']=""
+# Add required blank columns, then reorder
+
+# In[9]:
+
+
 df['Facilitator']=""
-df=df[['Category','Code','Course', 'Facilitator' , 'First Session','Sessions', 'Operating Fee']]
+df=df[['Code','Course', 'Facilitator' , 'First Session','Sessions', 'Tot-Stus','Operating Fee']]
 
-# Populate the category field (which is just the Code field stripped of digits)
 
-df['Category'] = df['Code'].replace('(\d+)','',regex=True)
+# In[10]:
 
-# Sort by start date and course name, ready to populate #weeks/month 
 
-df=df.sort_values(['Category','Course'])
+df
 
-# Export
+
+# In[11]:
+
+
+df.info()
+
+
+# In[12]:
+
 
 df.to_csv('hurrah.csv',index=False)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
